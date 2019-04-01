7519b7a5d5092cc999ceb1dfa0f4e6ae1cab060f
Piumi Fernando <pwarnaku@deakin.edu.au>
2019-04-01 20:50:30 +1100

    Background created

--- BlocksAndRun/BlocksAndRun/GameViewController.swift
+++ BlocksAndRun/BlocksAndRun/GameViewController.swift
@@ -23,11 +23,11 @@ class GameViewController: UIViewController {
             if let sceneNode = scene.rootNode as! GameScene? {
                 
                 // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
+             //   sceneNode.entities = scene.entities
+             //   sceneNode.graphs = scene.graphs
+             //
                 // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
+             //   sceneNode.scaleMode = .aspectFill
                 
                 // Present the scene
                 if let view = self.view as! SKView? {
