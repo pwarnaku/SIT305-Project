71ad0936a97a3d6f2d398020e1a1abbeb97aa631
Piumi Fernando <pwarnaku@deakin.edu.au>
2019-04-02 02:15:02 +1100

    implemented an unlimited moving bridge

--- BlocksAndRun/BlocksAndRun/GameScene.swift
+++ BlocksAndRun/BlocksAndRun/GameScene.swift
@@ -11,24 +11,38 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var movingBridge: MovingBridge!
+    
     // this function will run as soon as the screen loads-up [piumi]
     override func didMove(to view: SKView) {
         
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
         background.position = CGPoint(x: self.size.width/2, y: self.size.height/2) //get the center point for the position
-        
         background.zPosition = 0
         
         self.addChild(background) // makes the background
         
         
-        let player = SKSpriteNode(imageNamed: "player-run")
-        player.setScale(1)
-        player.position = CGPoint(x: self.size.width/3, y: self.size.height/5)
-        player.zPosition = 2
         
-        self.addChild(player)// creates the player
-
-}
+        movingBridge = MovingBridge(size: CGSize(width: self.size.width, height: 120))
+        
+        movingBridge.position = view.center
+        movingBridge.zPosition = 1
+        self.addChild(movingBridge)
+        
+        
+        // let player = SKSpriteNode(imageNamed: "player-run")
+        // player.setScale(1)
+        //  player.position = CGPoint(x: self.size.width/3, y: self.size.height/5)
+        //player.zPosition = 2
+        
+        // self.addChild(player)// creates the player
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        movingBridge.start() // this will call the funtion ( start)
+        
+    }
 }
---
+++ BlocksAndRun/BlocksAndRun/MovingBridge.swift
@@ -0,0 +1,9 @@
+//
+//  MovingBridge.swift
+//  BlocksAndRun
+//
+//  Created by Piumi on 2/4/19.
+//  Copyright © 2019 Deakin. All rights reserved.
+//
+
+import Foundation
